1324d5db73efb39850703862e8045e0e4939eb69
Ashas Ahmad <ashasahmad@hotmail.com>
2019-08-03 04:36:23 +0500

    added archival strategy in main program added save db table functionality in main program added logging in main program

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from alpha_api_parser import api_parser
+import sqlite3
+from db_init import create_connection
+import logging
+import os
+from time import sleep
+import datetime
+
+logging.basicConfig(level=logging.INFO)
 
 def main():
+    execution_date = datetime.datetime.now()
     # setting up api configs
     base_url = 'https://www.alphavantage.co/query?function={}&symbol={}&market={}&apikey={}'
     function = 'DIGITAL_CURRENCY_DAILY'
     symbol = 'BTC'
     market = 'CNY'
     apikey = 'demo'
+    table_name = 'crypto_daily'
+    sqlite_db = 'db/crypto.db'
+    archive_folder = 'archives'
+    sleep_seconds = 5
 
     #parent dictionary key for dataframe
     parent_dict_key = 'Time Series (Digital Currency Daily)'
@@ -16,12 +29,40 @@ def main():
 
     #using custom module calling api_parser
     df = api_parser(base_url, function, symbol, market, apikey, parent_dict_key, df_column_list)
-    
-    print(df.head(10))
 
 
-def save_in_db():
-    
+    #creating the directory structure
+    if not os.path.exists(archive_folder + '/' + symbol):
+        os.makedirs(archive_folder + '/' + symbol)
+
+    logging.info("Archiving the data")
+    sleep(sleep_seconds)
+    #archiving the file
+    df.to_csv(archive_folder + '/' + symbol + '/' + str(execution_date) + '.csv', index=False)
+
+    logging.info("Setting up database connection")
+    sleep(sleep_seconds)
+    conn = create_connection(sqlite_db)
+
+    logging.info("Saving the data in database table")
+    sleep(sleep_seconds)
+    #saving the df to db table
+    df.to_sql(table_name, conn, if_exists='replace', index=False)
+
+    logging.info("Testing the data by getting a sample of it")
+    sleep(sleep_seconds)
+    test_sample_data(conn, table_name)
+
+
+def test_sample_data(conn, table_name):
+    cursorObj = conn.cursor()
+    cursorObj.execute('SELECT * FROM '+table_name+' limit 2')
+ 
+    rows = cursorObj.fetchall()
+ 
+    for row in rows:
+        print(row)
+
 
 if __name__ == '__main__':
     main()
