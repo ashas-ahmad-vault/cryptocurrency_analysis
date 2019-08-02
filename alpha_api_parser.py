565d099b4f995a2fcae8e7d1df6733b19cd56d63
Ashas Ahmad <ashasahmad@hotmail.com>
2019-08-03 04:36:44 +0500

    added logging and sleep in alpha api parser module

--- alpha_api_parser.py
+++ alpha_api_parser.py
@@ -1,13 +1,24 @@
 import requests
 import json
 import pandas as pd
+import logging
+from time import sleep
+
+sleep_in_seconds = 5
+
+logging.basicConfig(level=logging.INFO)
 
 def api_parser(base_url = 'https://www.alphavantage.co/query?function={}&symbol={}&market={}&apikey={}', function = 'DIGITAL_CURRENCY_DAILY', symbol = 'BTC', market = 'CNY', apikey = 'demo', parent_dict_key = 'Time Series (Digital Currency Daily)', column_list = ['open_cny','open_usd','high_cny','high_usd','low_cny','low_usd','close_cny','close_usd','volume','market_cap','date']):
     # This function is responsible
     # of taking alpha advantage api configs
     # and returning a pandas dataframe
 
+    
+    logging.info("Going to make the API request")
+    sleep(sleep_in_seconds)
     response = requests.get(base_url.format(function, symbol, market, apikey))
+    logging.info("Request is successful")
+    sleep(sleep_in_seconds)
     data_dict = json.loads(response.text)
 
     df = pd.DataFrame(data_dict[parent_dict_key])
@@ -16,6 +27,8 @@ def api_parser(base_url = 'https://www.alphavantage.co/query?function={}&symbol=
     
     # setting up two more columns in dataframe
     # of date and coin 
+    logging.info("Setting up date and coin columns")
+    sleep(2)
     df['date'] = df.index
     df['coin'] = symbol
     
