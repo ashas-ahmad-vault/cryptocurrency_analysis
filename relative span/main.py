c5ea4f9de1d5aa388db86d1e2161bfeb0c881594
Ashas Ahmad <ashasahmad@hotmail.com>
2019-08-03 18:17:43 +0500

    added get weekly start end date logic

--- relative span/main.py
+++ relative span/main.py
@@ -1,10 +1,11 @@
 from alpha_api_parser import api_parser
-import sqlite3
 from db_init import create_connection
-import logging
+import sqlite3
+import pandas as pd
 import os
 from time import sleep
 import datetime
+import logging
 
 logging.basicConfig(level=logging.INFO)
 
@@ -18,9 +19,11 @@ def main():
     market = 'CNY'
     apikey = 'demo'
     table_name = 'crypto_daily'
-    sqlite_db = 'db/crypto.db'
+    sqlite_db = 'crypto.db'
     archive_folder = 'archives'
-    sleep_seconds = 5
+    sleep_seconds = 0
+    start_year = 2014
+    end_year = datetime.datetime.now().year
 
     #parent dictionary key for dataframe
     parent_dict_key = 'Time Series (Digital Currency Daily)'
@@ -51,12 +54,31 @@ def main():
     #saving the df to db table
     df.to_sql(table_name, conn, if_exists='replace', index=False)
 
+    logging.info(str(len(df)) + " records have been inserted in " + table_name)
+    sleep(sleep_seconds)
+
     logging.info("Testing the data by getting a sample of it")
     sleep(sleep_seconds)
     test_sample_data(conn, table_name)
 
+    get_relative_spans(start_year, end_year, df, conn, table_name)
+
     logging.info("Terminating!!")
     sleep(sleep_seconds)
+
+def get_relative_spans(start_year, end_year,  df, conn, table_name):
+    relative_span_dict = {}
+    week_counter = 1
+
+    df = pd.read_sql_query("SELECT * FROM " + table_name, conn)
+    df = df.set_index(['date'])
+    df.sort_index(inplace=True, ascending=True)
+
+    for year in range(start_year, end_year+1):
+        total_weeks = weeks_for_year(year)
+        for week in range(1, total_weeks + 1):
+            start, end = get_start_end_dates(year,week)
+            print(df.loc[start:end])
 
 
 def test_sample_data(conn, table_name):
@@ -68,6 +90,18 @@ def test_sample_data(conn, table_name):
     for row in rows:
         print(row)
 
+def get_start_end_dates(year, week):
+     d = datetime.date(year,1,1)
+     if(d.weekday()<= 3):
+         d = d - datetime.timedelta(d.weekday())             
+     else:
+         d = d + datetime.timedelta(7-d.weekday())
+     dlt = datetime.timedelta(days = (week-1)*7)
+     return str(d + dlt),  str(d + dlt + datetime.timedelta(days=6))
+
+def weeks_for_year(year):
+    last_week = datetime.date(int(year), 12, 28)
+    return last_week.isocalendar()[1]
 
 if __name__ == '__main__':
     main()
